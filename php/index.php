89fd444b784035f895fd217ae01371c49ab3177f
Gustavo Guimarães de Sousa <167660193+GustavoCodou@users.noreply.github.com>
2025-08-20 21:22:47 -0300

    first commit

---
+++ php/index.php
@@ -0,0 +1,26 @@
+<?php
+include "partials/header.php"
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/index.css"> <!-- aqui conecta o CSS -->
+</head>
+<body>
+
+<div class="overlay"></div>
+  <div class="conteudo">
+    <h2 id="titulo-principal" >Welcome to our PHP App</h2>
+    <h2 id="titulo-secundario" >Securely login and manage your account with us</h2>
+    <?php include "partials/navigation.php";?>
+  </div>
+
+</body>
+</html>
+
+
+<?php include "partials/footer.php";?>
